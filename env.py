850930ead400f2e6aea5da77cc30f2649edf60b1
chloejones9464 <jones.chloe@live.co.uk>
2025-06-14 15:45:54 +0100

    Connect database and add security

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_K6TFdCrsN2JQ@ep-aged-dream-a2z326dx.eu-central-1.aws.neon.tech/wife_basil_zebra_404774")
+
+os.environ.setdefault("SECRET_KEY", "5ea162f42fd4c0823d875e7d2e0f9f38")
