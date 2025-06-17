f968008baef7e2571fd66445bb689ad55d86d61f
chloejones9464 <jones.chloe@live.co.uk>
2025-06-17 12:20:54 +0100

    add template

--- blog/admin.py
+++ blog/admin.py
@@ -6,9 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'status', 'creadted_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'creadted_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
