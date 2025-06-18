656f692c284e27f4cd4a57778df5a1e511f798a0
chloejones9464 <jones.chloe@live.co.uk>
2025-06-18 19:04:50 +0100

    add about page

---
+++ about/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from .models import About
+
+# Create your views here.
+
+queryset = About.objects.all().order_by('-update_on').first()
+
+
+def about(request, about):
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
+
+
+
+
