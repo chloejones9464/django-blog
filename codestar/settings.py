3de314660d58c21f6eb602caf942e7b4b45291ac
chloejones9464 <jones.chloe@live.co.uk>
2025-06-21 16:34:11 +0100

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'blog',
     'django_summernote',
+    'cloudinary',
     'about',
     
 ]
--- env.py
+++ env.py
@@ -4,3 +4,7 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_K6TFdCrsN2JQ@ep-aged-dream-a2z326dx.eu-central-1.aws.neon.tech/wife_basil_zebra_404774")
 
 os.environ.setdefault("SECRET_KEY", "5ea162f42fd4c0823d875e7d2e0f9f38")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dcvyln5fy"
+)
